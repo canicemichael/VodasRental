afbeb1353822751cd66dea72753de77147fc0fc1
canicemichael <canicemichael@gmail.com>
2024-05-27 10:52:07 +0100

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -892,4 +892,28 @@ function deleteAllCookies() {
   }
 }
 
-deleteAllCookies();
+// deleteAllCookies();
+
+
+// button animation
+var buttons = document.querySelectorAll('.btn-dark');
+
+Array.prototype.forEach.call(buttons, function (b) {
+  b.addEventListener('click', createRipple)
+});
+
+function createRipple(event) {
+  var ripple = document.createElement('span');
+  ripple.classList.add('ripple');
+  
+  var max = Math.max(this.offsetWidth, this.offsetHeight);
+  
+  ripple.style.width = ripple.style.height = max*2 + 'px';
+  
+  var rect = this.getBoundingClientRect();
+  
+  ripple.style.left = event.clientX - rect.left - max + 'px';
+  ripple.style.top = event.clientY - rect.top - max + 'px';
+  
+  this.appendChild(ripple);
+}
