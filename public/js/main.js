076b9cf3c7d8ae01027ed0519ec3d65242c26b06
canicemichael <canicemichael@gmail.com>
2024-05-27 11:29:27 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -891,8 +891,10 @@ function deleteAllCookies() {
       document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
   }
 }
-
 // deleteAllCookies();
+
+var domCookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
+console.log(domCookies);
 
 
 // button animation
